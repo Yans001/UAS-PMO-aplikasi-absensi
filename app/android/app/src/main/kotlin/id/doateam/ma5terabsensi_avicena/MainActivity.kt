9679aab5b53582c958d9a220724f4bc99807bf05
Yans001 <riyanmudji23r@gmail.com>
2024-06-28 20:34:07 +0800

    update flutter app in main

---
+++ app/android/app/src/main/kotlin/id/doateam/ma5terabsensi_avicena/MainActivity.kt
@@ -0,0 +1,6 @@
+package id.doateam.ma5terabsensi_avicena
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app/android/app/src/main/kotlin/id/navetacandra/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package id.navetacandra.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
